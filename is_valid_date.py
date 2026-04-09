1a435585304b5c577e9839092157f682b4d87722
Musk <musk.1109@gmail.com>
2026-04-08 21:41:46 -0700

    Add is_valid_date

--- is_valid_date.py
+++ is_valid_date.py
@@ -1,27 +1,9 @@
 from days_in_month import days_in_month
 
-# Makoto version
-def is_valid_date(year, month, day):
-    if month not in range(1, 13):
-        return False
-    if day < 1:
-        return False
-    return day <= days_in_month(year, month)from days_in_month import days_in_month
-
-
+# Misk version
 def is_valid_date(year, month, day):
     if month < 1 or month > 12:
         return False
-    if day < 1 or day > days_in_month(year, month):
+    if day not in range(1, days_in_month(year, month) + 1):
         return False
     return True
-
-
-if __name__ == "__main__":
-    print(is_valid_date(2020, 2, 29))  # True
-    print(is_valid_date(2019, 2, 29))  # False
-    print(is_valid_date(2020, 1, 31))  # True
-    print(is_valid_date(2020, 4, 31))  # False
-    print(is_valid_date(1938, 8, 31)) # True
-    print(is_valid_date(1602, 9, 30)) # True
-    print(is_valid_date(1602, 9, 31)) # False
